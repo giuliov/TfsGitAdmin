b8def46e9927a2a542a7bc86dc1a72ced8969165
Giulio Vian <giuliovdev@gmail.com>
2016-01-19 16:29:56 +0100

    Added support for user+password on command line

--- GitAdminBaseCommand.cs
+++ GitAdminBaseCommand.cs
@@ -4,6 +4,7 @@ using Microsoft.TeamFoundation.Git.Client;
 using Microsoft.TeamFoundation.SourceControl.WebApi;
 using Microsoft.VisualStudio.Services.Common;
 using System;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace TfsGitAdmin
@@ -16,6 +17,8 @@ namespace TfsGitAdmin
         internal string TfsUrl { get; set; }
         internal string TeamProjectCollection { get; set; }
         internal string TeamProject { get; set; }
+        internal string Username { get; set; }
+        internal string Password { get; set; }
 
         public GitAdminBaseCommand()
         {
@@ -25,10 +28,14 @@ namespace TfsGitAdmin
               value => this.ShowHelp = value != null);
             this.HasRequiredOption("s|tfsUrl=",  "TFS Url, e.g. http://localhost:8080/tfs/",
               value => this.TfsUrl = value);
-            this.HasRequiredOption("c|teamProjectCollection=",  "Password for Source user",
+            this.HasRequiredOption("c|teamProjectCollection=",  "Team Project Collection",
               value => this.TeamProjectCollection = value);
-            this.HasRequiredOption("p|teamProject=",  "Source Project Name",
+            this.HasRequiredOption("p|teamProject=", "Source Project Name",
               value => this.TeamProject = value);
+            this.HasOption("u|username=", "Username",
+              value => this.Username = value);
+            this.HasOption("w|password=", "Password",
+              value => this.Password = value);
         }
 
         protected TfsTeamProjectCollection tpc;
@@ -38,14 +45,40 @@ namespace TfsGitAdmin
         protected void Connect()
         {
             string tpcUrl = TfsUrl + "/" + TeamProjectCollection + "/";
-            tpc = TfsTeamProjectCollectionFactory.GetTeamProjectCollection(new Uri(tpcUrl));
+
+            if (!string.IsNullOrWhiteSpace(this.Username))
+            {
+                Console.WriteLine("Authenticating as '{0}'", this.Username);
+
+                // source http://blogs.msdn.com/b/buckh/archive/2013/01/07/how-to-connect-to-tf-service-without-a-prompt-for-liveid-credentials.aspx
+                NetworkCredential netCred = new NetworkCredential(this.Username, this.Password);
+                BasicAuthCredential basicCred = new BasicAuthCredential(netCred);
+                TfsClientCredentials tfsCred = new TfsClientCredentials(basicCred);
+                tfsCred.AllowInteractive = false;
+
+                tpc = new TfsTeamProjectCollection(new Uri(tpcUrl), tfsCred);
+            }
+            else
+            {
+                tpc = new TfsTeamProjectCollection(new Uri(tpcUrl));
+            }
 
             tpc.EnsureAuthenticated();
 
             gitSvc = tpc.GetService<GitRepositoryService>();
 
-            var cred = new VssCredentials(true);
-            gitWebApiClient = new GitHttpClient(new Uri(tpcUrl), cred);
+            if (!string.IsNullOrWhiteSpace(this.Username))
+            {
+                NetworkCredential netCred = new NetworkCredential(this.Username, this.Password);
+                var cred = new VssCredentials(new Microsoft.VisualStudio.Services.Common.WindowsCredential(netCred));
+                gitWebApiClient = new GitHttpClient(new Uri(tpcUrl), cred);
+            }
+            else
+            {
+                var cred = new VssCredentials(true);
+                gitWebApiClient = new GitHttpClient(new Uri(tpcUrl), cred);
+            }
+
         }
 
         protected int WaitAsyncTask(Task task)
